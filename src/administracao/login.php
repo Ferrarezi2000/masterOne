d430b0bff5cc340a2083c8af29eb8a72a956b963
Thiago Ferrarezi <thiago.ferrarezi@sumicity.net.br>
2018-03-29 14:09:49 -0300

    php no blog

--- src/administracao/login.php
+++ src/administracao/login.php
@@ -9,15 +9,12 @@
     <meta property="og:description" content="Consultoria e inteligência em negócios" />
     <title>Master One</title>
 
-    <link href="../../assets/css/meu_css/login.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="../../assets/css/meu_css/login.css" rel="stylesheet">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
-
-
-
 <div class="container">
 
     <div class="col-md-10 col-md-offset-1 main" >
@@ -30,31 +27,31 @@
         </div><!--col-sm-6-->
 
         <div class="col-md-6 right-side">
-            <h3>Login</h3>
+            <h3 style="margin-bottom: 60px">Login</h3>
 
             <!--Form with header-->
             <div class="form">
+                <form method="post" action="../../php/login/login.php" id="formlogin" name="formlogin">
 
+                    <div class="form-group">
+                        <label for="form2">Usuário</label>
+                        <input type="text" class="form-control input-lg" placeholder="usuário" name="login" id="login">
 
-                <div class="form-group">
-                    <label for="form2">Your usersname / e-mail</label>
-                    <input type="text" id="form2" class="form-control input-lg">
+                    </div>
 
-                </div>
+                    <div class="form-group">
+                        <label for="form4">Senha</label>
+                        <input type="password" name="senha" id="senha" placeholder="senha" class="form-control input-lg">
 
-                <div class="form-group">
-                    <label for="form4">Your password</label>
-                    <input type="password" id="form4" class="form-control input-lg">
+                    </div>
 
-                </div>
+                    <div class="text-xs-center" >
+                        <input type="submit" class="btn btn-deep-purple" style="margin-top: 100px"/>
+                    </div>
 
-                <div class="text-xs-center">
-                    <button class="btn btn-deep-purple">Login</button>
-                </div>
-
-
+                </form>
             </div>
-            <!--/Form with header-->
+                <!--/Form with header-->
 
         </div><!--col-sm-6-->
 
@@ -63,34 +60,6 @@
 
 </div><!--container-->
 
-
-<!--<div class="container">-->
-<!--    <div class="row vertical-offset-100">-->
-<!--        <div class="col-md-4 col-md-offset-4">-->
-<!--            <div class="panel panel-default">-->
-<!--                <div class="panel-heading">-->
-<!--                    <h3 class="panel-title">Bem Vindo</h3>-->
-<!--                </div>-->
-<!--                <div class="panel-body">-->
-<!--                    <form accept-charset="UTF-8" role="form" method="post"-->
-<!--                          action="../../php/login/login.php" id="formlogin" name="formlogin">-->
-<!--                        <fieldset>-->
-<!--                            <div class="form-group">-->
-<!--                                <input class="form-control" placeholder="Nome" name="login" id="login" type="text">-->
-<!--                            </div>-->
-<!--                            <div class="form-group">-->
-<!--                                <input class="form-control" placeholder="Senha" type="password" value=""-->
-<!--                                        name="senha" id="senha">-->
-<!--                            </div>-->
-<!--                            <input class="btn btn-lg btn-success btn-block" type="submit" value="Login"-->
-<!--                                    style="background-color: royalblue !important;">-->
-<!--                        </fieldset>-->
-<!--                    </form>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
 
 </body>
 </html>
